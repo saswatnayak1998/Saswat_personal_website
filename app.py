afcc6e1f3f7ca4e8c2fe0ed94cc2dafec5901884
Saswat Nayak <sknayak@wisc.edu>
2024-03-31 02:56:14 -0500

    Chatbot Functioning fully

--- app.py
+++ app.py
@@ -11,41 +11,52 @@ from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from flask_cors import CORS
 import os
+import dill as pickle
 
 # instantiate Flask app and database
 app = Flask(__name__)
 CORS(app) 
 
 def chatbot(question):
-    llm = Ollama(model="llama2")
+    pickle_path = 'retrieval_chain.pkl'
+    if os.path.exists(pickle_path):
+        with open(pickle_path, 'rb') as file:
+            retrieval_chain = pickle.load(file)
+            print("WORKS")
+            response = retrieval_chain.invoke({"input": question})
+            return response["answer"]
 
+    else:
+        llm = Ollama(model="llama2")
+        output_parser = StrOutputParser()
+        
+        loader = WebBaseLoader("http://127.0.0.1:5500/about.html")
 
-    output_parser = StrOutputParser()
-    
-    loader = WebBaseLoader("http://127.0.0.1:5500/about.html")
+        docs = loader.load()
 
-    docs = loader.load()
+        embeddings = OllamaEmbeddings()
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(docs)
+        vector = FAISS.from_documents(documents, embeddings)
 
-    embeddings = OllamaEmbeddings()
-    text_splitter = RecursiveCharacterTextSplitter()
-    documents = text_splitter.split_documents(docs)
-    vector = FAISS.from_documents(documents, embeddings)
+        prompt = ChatPromptTemplate.from_template("""you are Saswat, i.e. the person in the text. Answer questions like saswat will answer. He respects people and is funny sometimes.Never mention that you are an AI. 
 
-    prompt = ChatPromptTemplate.from_template("""you are Saswat, i.e. the person in the website. Answer questions like saswat will answer
+        <context>
+        {context}
+        </context>
 
-    <context>
-    {context}
-    </context>
+        Question: {input}""")
 
-    Question: {input}""")
+        document_chain = create_stuff_documents_chain(llm, prompt)
 
-    document_chain = create_stuff_documents_chain(llm, prompt)
+        retriever = vector.as_retriever()
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        response = retrieval_chain.invoke({"input": question})
+        with open(pickle_path, 'wb') as file:
+            pickle.dump(retrieval_chain, file)
+            print("Fuckin_Works")
 
-    retriever = vector.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    response = retrieval_chain.invoke({"input": question})
-
-    return response["answer"]
+            return response["answer"]
 
 @app.route("/ask", methods=['POST'])
 def askme():
